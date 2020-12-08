66b5863f9d706a2d4814f1f6f7687ed170494148
ANANYAVINOD <ananya@qburst.com>
2020-12-08 14:34:28 +0530

    Added custom style for homepage

---
+++ zakra/footer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Zakra
+ * @since Zakra 1.0
+ */
+
+?>
+
+<footer role="contentinfo" class="footer">
+	<div class="footer-sec-one">
+		<ul class="footer-contents flex-box container">
+		    <li class="contents-list">
+		        <div class="logo-sec flex-box">
+		            <a href="#" title="Zakra"><i class="fa fa-bullseye"></i></a>
+			        <a href="#" title="Zakra"><h1 class="logo-title">Zakra</h1></a>
+			    </div>
+			    <div class="about-sec">
+				    <p>Zakra is a modern multipurpose theme that comes with 10+ free starter sites
+				       to make your site beautiful and professional.</p>
+			    </div>
+		    </li>
+		    <li class="contents-list">
+	     	    <ul>
+				    <li>Documentation</li>
+				    <li>Installation</li>
+		    	    <li>Getting started</li>
+				    <li>Advanced usage</li>
+				    <li>Custom development</li>
+			    </ul>
+		    </li>
+		    <li class="contents-list">
+			    <ul>
+				    <li>Company</li>
+				    <li>Team</li>
+				    <li>About us</li>
+				    <li>Blog</li>
+			    </ul>
+		    </li> 
+		    <li class="contents-list">
+			    <ul>
+				    <li>Social</li>
+				    <li>Facebook</li>
+				    <li>Linkedin</li>
+				    <li>Twitter</li>
+				    <li>Google Plus</li>
+			    </ul>
+		    </li>  
+	    </ul>
+	</div>
+	<div class="footer-sec-two">
+		<div class="container">
+			<p>Copyright  &copy 2020</p>
+		</div>
+	</div>
+
+</footer>
+
+</body>
+</html>
